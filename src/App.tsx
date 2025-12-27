7a6fe990732e715d6563546d709a04fa48036a52
MakarevichAV <makarevich.a.v@yandex.ru>
2025-12-27 13:23:06 +0200

    Implemented token dependent routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
 import './App.css'
 import Profile from "./components/Profile";
-import {Route, Routes} from "react-router";
+import {Navigate, Route, Routes} from "react-router";
 import Guest from "./components/Guest";
 
 function App() {
-  // TODO: Implement token retrieval from global state logic
-  const token = '';
-  return (
-      <Routes>
-        <Route path="/" element={<Guest/>} />
-        <Route path="/profile" element={<Profile/>} />
-      </Routes>
+    // TODO: Implement token retrieval from global state logic
+    const token = '';
+    return (
+        <Routes>
+            <Route
+                path="/"
+                element={
+                    token ? <Navigate to="/profile" replace/> : <Guest/>
+                }
+            />
+            <Route
+                path="/profile"
+                element={
+                    token ? <Profile/> : <Navigate to="/" replace/>
+                }
+            />
+        </Routes>
 
-  )
+    )
 }
 
 export default App
