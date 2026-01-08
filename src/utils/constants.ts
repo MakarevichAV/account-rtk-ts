ce513ac1b208c3abdd3f51278ca2781253746b40
MakarevichAV <makarevich.a.v@yandex.ru>
2026-01-08 20:28:09 +0200

    save

--- src/utils/constants.ts
+++
@@ -1,8 +0,0 @@
-export const UPDATE_MODE_DAFAULT = "UPDATE_MODE_DAFAULT";
-export const UPDATE_MODE_CHANGE_PASSWORD = "UPDATE_MODE_CHANGE_PASSWORD";
-export const UPDATE_MODE_EDIT_PROFILE = "UPDATE_MODE_EDIT_PROFILE";
-
-export const baseUrl = 'https://webaccounting.herokuapp.com'
-export const createToken = (login: string, password: string) => {
-    return 'Basic ' + btoa(`${login}:${password}`)
-}
